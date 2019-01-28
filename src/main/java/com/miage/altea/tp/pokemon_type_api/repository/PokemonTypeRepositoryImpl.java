ea2e4c6ad1ec39635392c2ba5d305063753855f8
SAUVALLE Damien <damien.sauvalle.etu@univ-lille.fr>
2019-01-28 10:43:45 +0100

    PokeType done

---
+++ src/main/java/com/miage/altea/tp/pokemon_type_api/repository/PokemonTypeRepositoryImpl.java
@@ -0,0 +1,42 @@
+package com.miage.altea.tp.pokemon_type_api.repository;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.miage.altea.tp.pokemon_type_api.bo.PokemonType;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+public class PokemonTypeRepositoryImpl implements PokemonTypeRepository {
+
+    private List<PokemonType> pokemons;
+
+    public PokemonTypeRepositoryImpl() {
+        try {
+            var pokemonsStream = this.getClass().getResourceAsStream("/pokemons.json");
+
+            var objectMapper = new ObjectMapper();
+            var pokemonsArray = objectMapper.readValue(pokemonsStream, PokemonType[].class);
+            this.pokemons = Arrays.asList(pokemonsArray);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public PokemonType findPokemonTypeById(int id) {
+        System.out.println("Loading Pokemon information for Pokemon id " + id);
+        return this.pokemons.stream().filter(x -> x.getId() == id).findFirst().orElse(null);
+    }
+
+    @Override
+    public PokemonType findPokemonTypeByName(String name) {
+        System.out.println("Loading Pokemon information for Pokemon name " + name);
+        return this.pokemons.stream().filter(x -> name.equals(x.getName())).findFirst().orElse(null);
+    }
+
+    @Override
+    public List<PokemonType> findAllPokemonType() {
+        return this.pokemons;
+    }
+}
---
+++ src/test/java/com/miage/altea/pokemon_type_api/repository/PokemonTypeRepositoryImplTest/PokemonTypeRepositoryImplTest.java
@@ -0,0 +1,53 @@
+package com.miage.altea.pokemon_type_api.repository.PokemonTypeRepositoryImplTest;
+
+import com.miage.altea.tp.pokemon_type_api.repository.PokemonTypeRepositoryImpl;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class PokemonTypeRepositoryImplTest {
+
+    private PokemonTypeRepositoryImpl repository = new PokemonTypeRepositoryImpl();
+
+    @Test
+    void findPokemonTypeById_with25_shouldReturnPikachu(){
+        var pikachu = repository.findPokemonTypeById(25);
+        assertNotNull(pikachu);
+        assertEquals("pikachu", pikachu.getName());
+        assertEquals(25, pikachu.getId());
+    }
+
+    @Test
+    void findPokemonTypeById_with145_shouldReturnZapdos(){
+        var zapdos = repository.findPokemonTypeById(145);
+        assertNotNull(zapdos);
+        assertEquals("zapdos", zapdos.getName());
+        assertEquals(145, zapdos.getId());
+    }
+
+    @Test
+    void findPokemonTypeByName_withEevee_shouldReturnEevee(){
+        var eevee = repository.findPokemonTypeByName("eevee");
+        assertNotNull(eevee);
+        assertEquals("eevee", eevee.getName());
+        assertEquals(133, eevee.getId());
+    }
+
+    @Test
+    void findPokemonTypeByName_withMewTwo_shouldReturnMewTwo(){
+        var mewtwo = repository.findPokemonTypeByName("mewtwo");
+        assertNotNull(mewtwo);
+        assertEquals("mewtwo", mewtwo.getName());
+        assertEquals(150, mewtwo.getId());
+    }
+
+    @Test
+    void findAllPokemonType_shouldReturn151Pokemons(){
+        var pokemons = repository.findAllPokemonType();
+        assertNotNull(pokemons);
+        assertEquals(151, pokemons.size());
+    }
+
+}
+
