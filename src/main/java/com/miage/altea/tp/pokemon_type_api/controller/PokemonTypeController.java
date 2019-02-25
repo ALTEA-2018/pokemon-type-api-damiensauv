c249457edc1bef3df1988bbcbec6edf3619016c5
SAUVALLE Damien <damien.sauvalle.etu@univ-lille.fr>
2019-02-25 13:14:10 +0100

    wip

--- src/main/java/com/miage/altea/tp/pokemon_type_api/controller/PokemonTypeController.java
+++ src/main/java/com/miage/altea/tp/pokemon_type_api/controller/PokemonTypeController.java
@@ -3,10 +3,12 @@ package com.miage.altea.tp.pokemon_type_api.controller;
 import com.miage.altea.tp.pokemon_type_api.bo.PokemonType;
 import com.miage.altea.tp.pokemon_type_api.service.PokemonTypeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 @RestController
 @RequestMapping("/pokemon-types")
@@ -21,6 +23,7 @@ class PokemonTypeController {
 
     @GetMapping("/{id}")
     PokemonType getPokemonTypeFromId(@PathVariable int id) {
+        Locale locale = LocaleContextHolder.getLocale();
         return this.pokemonTypeService.getPokemonType(id);
     }
 
